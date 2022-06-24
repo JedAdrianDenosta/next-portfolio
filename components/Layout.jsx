cedc6d757c8febcd5dc2b9074258e13782168dbb
Blankeos <carloantonioct@gmail.com>
2022-06-24 20:37:15 +0800

    Updated Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Nav />
-      <main className="bg-red-300 min-h-screen">{children}</main>
+      <main className="bg-red-300 min-h-screen h-full">{children}</main>
       <Footer />
     </div>
   );
