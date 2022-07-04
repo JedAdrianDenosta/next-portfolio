4f3835e30ae13f3eeb51c4b5a6961db18b1ea616
Jed Adrian Denosta <59909864+JedAdrianDenosta@users.noreply.github.com>
2022-07-05 02:39:27 +0800

    added optimization
    added optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
