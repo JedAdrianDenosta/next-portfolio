a0f0ee51168b8dc31b324d13436b650504449286
Jed Adrian Denosta <59909864+JedAdrianDenosta@users.noreply.github.com>
2022-08-02 01:15:10 +0800

    Update yarn

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "../components/Head";
-import Image from "next/image";
 import HeroSection from "../components/pages/home/HeroSection";
 import SkillsSection from "../components/pages/home/SkillsSection";
 import ProjectsSection from "../components/pages/home/ProjectsSection";
